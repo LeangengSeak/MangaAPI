35656cb079d161c9940c2736529a19cb075968dc
LeangengSeak <leangeng03@gmail.com>
2025-06-16 22:24:05 +0700

    Add TypeScript definitions for Express user and request interfaces

---
+++ src/@types/index.d.ts
@@ -0,0 +1,12 @@
+import { UserDocument } from "../models/user.model";
+import { Request } from "express";
+
+declare global {
+  namespace Express {
+    interface User extends UserDocument {}
+    interface Request {
+      user?: UserDocument;
+      sessionId?: string;
+    }
+  }
+}
