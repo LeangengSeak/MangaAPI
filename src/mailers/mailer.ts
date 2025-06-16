5d16680d6edd5550f5cbcf5114446a7cb7ebd42a
LeangengSeak <leangeng03@gmail.com>
2025-06-16 22:27:41 +0700

    Refactor mailer.ts: Change interface to type for Params and fix mailer_sender declaration

--- src/mailers/mailer.ts
+++ src/mailers/mailer.ts
@@ -1,26 +1,26 @@
 import { config } from "../config/app.config";
 import { resend } from "./resendClient";
 
-interface Params {
+type Params = {
   to: string | string[];
   subject: string;
   text: string;
   html: string;
   from?: string;
-}
+};
 
-const mail_sender =
+const mailer_sender =
   config.NODE_ENV === "development"
     ? `no-reply <onboarding@resend.dev>`
-    : `no-reply <${config.MAILER_SENDER}`;
+    : `no-reply <${config.MAILER_SENDER}>`;
 
 export const sendEmail = async ({
   to,
-  from = mail_sender,
+  from = mailer_sender,
   subject,
   text,
   html,
-}: Params) => {
+}: Params) =>
   await resend.emails.send({
     from,
     to: Array.isArray(to) ? to : [to],
@@ -28,4 +28,3 @@ export const sendEmail = async ({
     subject,
     html,
   });
-};
