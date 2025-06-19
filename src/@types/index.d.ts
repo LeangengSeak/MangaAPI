64ba282452b3edd0a6b11c21d16e607b8805a35e
LeangengSeak <leangeng03@gmail.com>
2025-06-20 00:17:09 +0700

    Refactor user type in Express Request interface to use User type

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,11 +1,13 @@
+import { UserPreferences } from "typescript";
 import { UserDocument } from "../models/user.model";
 import { Request } from "express";
+import { Role } from "../shared/utils/role-hierarchy";
 
 declare global {
   namespace Express {
     interface User extends UserDocument {}
     interface Request {
-      user?: UserDocument;
+      user?: User;
       sessionId?: string;
     }
   }
